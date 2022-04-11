7d891887adc41631d2f19c34826621b519084001
kingsmen1 <90970163+kingsmen1@users.noreply.github.com>
2022-04-12 01:07:51 +0530

    EP17

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:block_eccomerce_app/blocs/wishlist/wishlist_bloc.dart';
 import 'package:block_eccomerce_app/config/routes.dart';
 import 'package:block_eccomerce_app/config/theme.dart';
 import 'package:block_eccomerce_app/screens/splash/splash_screen.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() async{
-  // Bloc.observer= SimpleBlocObserver();
-  runApp( MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //app initializing
+  await Firebase.initializeApp(); //app initializing
+  //Bloc.observer= SimpleBlocObserver();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
